218bc5aa7db9390aa8ad2e9068f32c444f565639
SunyoungLim <gsw0981@naver.com>
2020-08-21 16:56:56 +0900

    0821study2

--- src/Q3.java
+++ src/Q3.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,16 +13,31 @@ public class Q3 {
 		paramMap.put("boWriter", "말자");
 		FreeBoardVO vo = new FreeBoardVO();
 		// paramMap에 저장된 정보를 vo 에 담는 처리 또는 함수 생성 후 호출
+
 		Field[] allFields = vo.getClass().getDeclaredFields();
 		for (Field field : allFields) {
-			System.out.print("getName: " + field.getName());
+			System.out.print("getName=> " + field.getName());
 			field.setAccessible(true);
-			Object value = field.get(vo);
-			System.out.println("value: " + value);
-			paramMap.put(field.getName(), value);
+//			Object value = field.get(vo);
+			System.out.println();
+//			paramMap.put(field.getName(), field.get(obj)value)
+//			Object value = paramMap.get(field.getName());
+			System.out.println("value: " + paramMap.get(field.getName()));
+			System.out.println("name: " + field.getType().getSimpleName());
+			switch(field.getType().getSimpleName()) {
+			case "int":
+				field.set(field.getName(), (int)paramMap.get(field.getName()));
+				break;
+			case "String":
+				field.set(field.getName(), (String)paramMap.get(field.getName()));
+				break;
+			}
+//			field.set(field.getName(), paramMap.get(field.getName()));
+			
+//			paramMap.put(field.getName(), value);
 			System.out.println(paramMap);
 		}
-//		System.out.println(paramMap);
+		System.out.println(paramMap);
 		System.out.printf("Info : %d %s %s\n", vo.getBoNo(), vo.getBoWriter(), vo.getBoTitle());
 	}
 
